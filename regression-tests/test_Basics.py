d45354c77c8467dbceea3dc42fae2a438e90fe26
Peter van Dijk <peter.van.dijk@powerdns.com>
2015-11-05 19:29:13 +0000

    first stab at regression tests

---
+++ regression-tests/test_Basics.py
@@ -0,0 +1,15 @@
+import requests
+import socket
+import time
+from test_helper import ApiTestCase
+
+
+class TestBasics(ApiTestCase):
+
+    def test_unauth(self):
+        r = requests.get(self.url("/?command=stats"))
+        self.assertEquals(r.status_code, requests.codes.unauthorized)
+
+    def test_auth_stats(self):
+	print "ohai"
+        r = self.session.get(self.url("/?command=stats"))
